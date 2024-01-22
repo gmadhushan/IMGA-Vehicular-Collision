274def438efa78c19a17b034da2be2beeda120d5
G. Madhumitha <62023065+gmadhushan@users.noreply.github.com>
2024-01-22 17:26:49 +0530

    IMGA_test.py

--- IMGA_test.py
+++ IMGA_test.py
@@ -37,11 +37,15 @@ batchsize = 1
 num_classes = 2
 att_type = 'two'
 mask_guided = False
-model_name = 'vgg19'
+model_name = 'vgg19' # Feature extracting backbone network
+#model_name = 'densenet161'
+#model_name = 'efficientnet_v2_l'
+#model_name = 'mobilenet_v2'
 
-data_dir = "IMGA_test/vehicle_collision"
+data_dir = "IMGA_test/vehicle_collision" # Specify the path of the test data
 
-class Leafvein(Dataset):
+#%%
+class VehicleCollision(Dataset):
     
     def __init__(self,
                  data_dir,
@@ -58,9 +62,8 @@ class Leafvein(Dataset):
         @ sample_n:    if not None, only use that many observations
         """
         self.data_dir = data_dir
-        #self.transform = transform
         self.mode=mode
-        #self.dataset=args.dataset
+       
         with open(os.path.join(self.data_dir,'labels.pkl'),'rb') as df:
             self.label=pickle.load(df)
         self.img_files=os.listdir(os.path.join(self.data_dir, self.mode))
@@ -71,18 +74,15 @@ class Leafvein(Dataset):
         self.rotate=rotate
         if mode=='train':
             self.transforms=transforms.Compose([
-            #transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.5),
             transforms.ToTensor(),
-            # the mean and std for leafvein dataset
+            # the mean and std for VehicleCollision dataset
             transforms.Normalize([0.485, 0.456, 0.406],
                                  [0.229, 0.224, 0.225])
             ])
         else:
             self.transforms=transforms.Compose([
-            #transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.5),
-            #transforms.Resize((720,1280)),
             transforms.ToTensor(),
-            # the mean and std for leafvein dataset
+            # the mean and std for VehicleCollision dataset
             transforms.Normalize([0.485, 0.456, 0.406],
                                  [0.229, 0.224, 0.225])
             ])
@@ -97,7 +97,6 @@ class Leafvein(Dataset):
         for file_name in self.img_files:
             img = PIL.Image.open(os.path.join(self.data_dir,self.mode, file_name))
             img = self.transforms(img)
-            #id_ = file_name.split('.')[0]
             id_ = file_name
             id2image[id_] = img
         
@@ -105,71 +104,27 @@ class Leafvein(Dataset):
     
     def __getitem__(self, idx):
         file_name = self.img_files[idx]
-        #id_=(file_name.split('.')[0])
         id_=(file_name)
-        #img = PIL.Image.open(os.path.join(self.data_dir, self.dataset, self.mode,file_name)).convert('RGB')
         img = PIL.Image.open(os.path.join(self.data_dir,self.mode,file_name)).convert('RGB')
-        #mask = PIL.Image.open(os.path.join(self.data_dir,'l2_mask',id_))
-        #label = self.label[int(id_)]-1
         label = self.label[id_]
-        #label = self.label[file_name]
         
-        """
-        if self.dataset=='btf':
-            mask = PIL.Image.open(os.path.join(self.data_dir,self.dataset,'l2_mask',file_name)) # for btf dataset
-            label=self.label[file_name]-1 # for btf dataset  
-        else:
-            mask = PIL.Image.open(os.path.join(self.data_dir,self.dataset,'l2_mask',id_+'.png')) # for soybean and hainan leaf dataset
-            label=self.label[int(id_)]-1 # for soybean and hainan leaf dataset
-        
-        if self.hflip:
-            if random.random() < 0.5:
-                img = img.transpose(PIL.Image.FLIP_LEFT_RIGHT)
-                mask = mask.transpose(PIL.Image.FLIP_LEFT_RIGHT)
-                
-        if self.vflip:
-            if random.random() < 0.5:
-                img = img.transpose(PIL.Image.FLIP_TOP_BOTTOM)
-                mask = mask.transpose(PIL.Image.FLIP_TOP_BOTTOM)"""
         img = self.transforms(img)
-        #mask = torch.FloatTensor(np.array(mask).astype(np.float))
-        #mask = torch.FloatTensor(np.array(mask).astype(np.float64))
-        
-        """    
-        if self.crop:
-            # perform random crop
-            h, w = img.shape[1], img.shape[2]
-            pad_tb = max(0, self.crop[0] - h)
-            pad_lr = max(0, self.crop[1] - w)
-            img = torch.nn.ZeroPad2d((0, pad_lr, 0, pad_tb))(img)
-            h, w = img.shape[1], img.shape[2]
-            i = random.randint(0, h - self.crop[0])
-            j = random.randint(0, w - self.crop[1])
-            img = img[:, i:i + self.crop[0], j:j + self.crop[1]]
-            if self.mode=='train':
-                mask = torch.nn.ConstantPad2d((0, pad_lr, 0, pad_tb), 0)(mask)
-                mask = mask[i:i + self.crop[0], j:j + self.crop[1]]
-        if self.erase:
-            if random.random() < 0.5:
-                i, j, h, w, v=transforms.RandomErasing.get_params(img,scale=(0.02, 0.33), ratio=(0.3, 3.3))
-                img = Ft.erase(img,i,j,h,w,v)
-                mask = Ft.erase(mask.unsqueeze(0),i,j,h,w,v).squeeze(0)"""
         return img,label
     
     def __len__(self):
         return len(self.img_files)
 
+#%%
 # Data
 print('==> Preparing data..')
 
-#test = Leafvein(args,mode='test')
-test = Leafvein(data_dir,mode='test')
-
+test = VehicleCollision(data_dir,mode='test')
 testloader = DataLoader(test, batch_size=batchsize, shuffle=False, num_workers=1)
+
 #%%
-class MGANET(nn.Module):
+class IMGANET(nn.Module):
     def __init__(self,backbone_name,num_classes,att_type=None,mask_guided=False):
-        super(MGANET, self).__init__()
+        super(IMGANET, self).__init__()
         
         self.mask_guided=mask_guided
         if backbone_name=='densenet161':
@@ -255,7 +210,7 @@ class MGANET(nn.Module):
         out = torch.flatten(out, 1)
         out = self.classifier(out)
         
-        #### for calculating mse loss
+        # for calculating mse loss
         if self.mask_guided:
             h,w = fg_att.shape[2],fg_att.shape[3]
             mask=F.adaptive_avg_pool2d(mask, (h, w))
@@ -321,28 +276,25 @@ class BasicConv(nn.Module):
         if self.relu is not None:
             x = self.relu(x)
         return x
+
 #%%
 # Model
 print('==> Building model..')
 
-model=MGANET(backbone_name=model_name,num_classes=num_classes,att_type=att_type,mask_guided=mask_guided)
+model=IMGANET(backbone_name=model_name,num_classes=num_classes,att_type=att_type,mask_guided=mask_guided)
 #print(model)
 net = model.to(device)
-checkpoint_path = '/home/madhu-phd/1_Image_Classification/2_Class_Classification/Mask_Guided_Dataset/output_vgg19/checkpoint/mga_vgg19_lr1e4_bs_8.pth'
+checkpoint_path = 'IMGA/output/checkpoint/imga_vgg19_lr1e4_bs_8.pth' # Give the path of the saved model file to load the model for running inference
 if device == 'cuda':
     net = torch.nn.DataParallel(net)
     cudnn.benchmark = True
 checkpoint = torch.load(checkpoint_path)
 net.load_state_dict(checkpoint['net'])
-# to print the model summary
-#import torchvision
-#from torchinfo import summary
-#summary(model)
-#summary(net))
+
 #%%
 test_acc=[]
 test_ce_losses=[]
-test_mse_losses=[]
+#test_mse_losses=[]
 output_pkldata = {}
 y_gt = []
 
@@ -351,7 +303,7 @@ criterion = nn.CrossEntropyLoss()
 net.eval()
 test_loss = 0
 ce_loss = 0
-mse_loss = 0
+#mse_loss = 0
 correct = 0
 total = 0
 with torch.no_grad():
@@ -376,7 +328,7 @@ with torch.no_grad():
         #inputs,targets,outputs=None,None,None
 
 #save the output predictions of entire test set as a pkl file
-with open('/home/madhu-phd/1_Image_Classification/2_Class_Classification/Mask_Guided_Dataset/output_vgg19/test_inference_lr1e4_bs_8.pkl','wb') as df:
+with open('IMGA/output/imga_vgg19_test_inference.pkl','wb') as df: # Give the name and path of the inference file to be saved
     pickle.dump(output_pkldata,df)
     
 y_true = torch.Tensor(y_gt) #convert all ground truths to a tensor
@@ -389,11 +341,8 @@ disp = ConfusionMatrixDisplay(confusion_matrix = cm)
     
 disp.plot()
 
-#report = classification_report(y_true, y_pred,
-#        target_names = ['No_Collision (Class 0)','Collision (Class 1)', 'Collided (Class 2)'])
 report = classification_report(y_true, y_pred,
         target_names = ['No_Collision (Class 0)','Collision (Class 1)'])
-
 
 print("Confusion Matrix")
 print(disp.confusion_matrix)
@@ -401,11 +350,10 @@ print(disp.confusion_matrix)
 print('\nClassification Report')
 print(report)        
      
-# Save checkpoint.
 acc = 100.*correct/total
 
 test_ce_losses.append(ce_loss/(batch_idx+1))
-test_mse_losses.append(mse_loss/(batch_idx+1))
+#test_mse_losses.append(mse_loss/(batch_idx+1))
 test_acc.append(acc)
 #print('cur_acc:{0},best_acc:{1}:'.format(acc,best_acc))
 print("finished")
@@ -413,7 +361,7 @@ print("Accuracy:{}".format(test_acc))
 print("Cross-Entropy Loss:{}".format(test_ce_losses))
 #print("Mean Square Error:{}".format(test_mse_losses))
 
-inference_file = open('/home/madhu-phd/1_Image_Classification/2_Class_Classification/Mask_Guided_Dataset/output_vgg19/inference_summary.txt', 'w')
+inference_file = open('IMGA/output/imga_vgg19_inference_summary.txt', 'w') # Give the name and path of the inference summary text file to be saved
 inference_file.write("\n Confusion Matrix:\n")
 inference_file.write(str(cm))
 inference_file.write("\nClassification Report:\n")
@@ -423,3 +371,4 @@ inference_file.write(txt1)
 txt2 = "\n Cross Entropy Loss for Test Data:{}".format(test_ce_losses)
 inference_file.write(txt2)
 inference_file.close()
+#%%
